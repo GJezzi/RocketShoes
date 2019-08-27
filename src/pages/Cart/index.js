491d714dd556eb70970274b35149b5ebe566cdce
GJezzi <gjezzi@hotmail.com>
2019-08-27 15:30:06 -0300

    Estilização da Home e Cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,77 @@
 import React from 'react';
+import {
+    MdRemoveCircleOutline,
+    MdAddCircleOutline,
+    MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-    return <div />;
+    return (
+        <Container>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th />
+                        <th>PRODUTO</th>
+                        <th>QTD</th>
+                        <th>SUBTOTAL</th>
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img
+                                src="https://images.lojanike.com.br/320x320/produto/99904_783137_20190704203531.png"
+                                alt="Tênis Masculino"
+                            />
+                        </td>
+                        <td>
+                            <strong>Tênis Jordan Air</strong>
+                            <span>R$129,90</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                                <input
+                                    type="number"
+                                    readOnly
+                                    value={1}
+                                    textAlign="center"
+                                />
+                                <button type="button">
+                                    <MdAddCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>R$380,00</strong>
+                        </td>
+                        <td>
+                            <button type="button">
+                                <MdDelete size={20} color="#7159c1" />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTable>
+            <footer>
+                <button type="button">Finalizar Pedido</button>
+                <Total>
+                    <span>TOTAL</span>
+                    <strong>R$1500,00</strong>
+                </Total>
+            </footer>
+        </Container>
+    );
 }
