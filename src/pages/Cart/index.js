eae8ea82ac937453d5945e505c2b098a25246f1c
GJezzi <gjezzi@hotmail.com>
2019-09-03 17:27:23 -0300

    RocketShoes Finalizado

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import {
     MdRemoveCircleOutline,
     MdAddCircleOutline,
     MdDelete,
 } from 'react-icons/md';
 
+import { formatPrice } from '../../utils/format';
+
+import * as CartActions from '../../store/modules/cart/actions';
+
 import { Container, ProductTable, Total } from './styles';
 
-export default function Cart() {
+function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
+    function increment(product) {
+        updateAmountRequest(product.id, product.amount + 1);
+    }
+
+    function decrement(product) {
+        updateAmountRequest(product.id, product.amount - 1);
+    }
+
     return (
         <Container>
             <ProductTable>
@@ -21,52 +35,80 @@ export default function Cart() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                            <img
-                                src="https://images.lojanike.com.br/320x320/produto/99904_783137_20190704203531.png"
-                                alt="Tênis Masculino"
-                            />
-                        </td>
-                        <td>
-                            <strong>Tênis Jordan Air</strong>
-                            <span>R$129,90</span>
-                        </td>
-                        <td>
-                            <div>
-                                <button type="button">
-                                    <MdRemoveCircleOutline
-                                        size={20}
-                                        color="#7159c1"
+                    {cart.map(product => (
+                        <tr>
+                            <td>
+                                <img src={product.image} alt={product.title} />
+                            </td>
+                            <td>
+                                <strong>{product.title}</strong>
+                                <span>{product.priceFormatted}</span>
+                            </td>
+                            <td>
+                                <div>
+                                    <button type="button">
+                                        <MdRemoveCircleOutline
+                                            size={20}
+                                            color="#7159c1"
+                                            onClick={() => decrement(product)}
+                                        />
+                                    </button>
+                                    <input
+                                        type="number"
+                                        readOnly
+                                        value={product.amount}
                                     />
+                                    <button type="button">
+                                        <MdAddCircleOutline
+                                            size={20}
+                                            color="#7159c1"
+                                            onClick={() => increment(product)}
+                                        />
+                                    </button>
+                                </div>
+                            </td>
+                            <td>
+                                <strong>{product.subtotal}</strong>
+                            </td>
+                            <td>
+                                <button
+                                    type="button"
+                                    onClick={() => removeFromCart(product.id)}
+                                >
+                                    <MdDelete size={20} color="#7159c1" />
                                 </button>
-                                <input type="number" readOnly value={1} />
-                                <button type="button">
-                                    <MdAddCircleOutline
-                                        size={20}
-                                        color="#7159c1"
-                                    />
-                                </button>
-                            </div>
-                        </td>
-                        <td>
-                            <strong>R$380,00</strong>
-                        </td>
-                        <td>
-                            <button type="button">
-                                <MdDelete size={20} color="#7159c1" />
-                            </button>
-                        </td>
-                    </tr>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </ProductTable>
             <footer>
                 <button type="button">Finalizar Pedido</button>
                 <Total>
                     <span>TOTAL</span>
-                    <strong>R$1500,00</strong>
+                    <strong>{total}</strong>
                 </Total>
             </footer>
         </Container>
     );
 }
+
+const mapStateToProps = state => ({
+    cart: state.cart.map(product => ({
+        ...product,
+        subtotal: formatPrice(product.price * product.amount),
+    })),
+    total: formatPrice(
+        state.cart.reduce((total, product) => {
+            return total + product.price * product.amount;
+        }, 0)
+    ),
+});
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators(CartActions, dispatch);
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Cart);
